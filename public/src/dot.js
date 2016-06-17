6e771e0e9e59107bc8fee5805fdae7fc1dfd6c5a
patrickmaley <pmaley@email.arizona.edu>
2016-06-16 20:00:37 -0700

    Initial push with working copy of game

---
+++ public/src/dot.js
@@ -0,0 +1,47 @@
+﻿require([], function () {
+  // Create the Dot sprite
+      Q.Sprite.extend("Dot", {
+        init: function(p) {
+          this._super(p,{
+            sheet: 'dot',
+            type: SPRITE_DOT,
+            // Set sensor to true so that it gets notified when it's
+            // hit, but doesn't trigger collisions itself that cause
+            // the player to stop or change direction
+            sensor: true
+          });
+
+          this.on("sensor");
+          this.on("inserted");
+        },
+
+        // When a dot is hit..
+        sensor: function() {
+          // Destroy it and keep track of how many dots are left
+          this.destroy();
+          this.stage.dotCount--;
+          // If there are no more dots left, just restart the game
+          if(this.stage.dotCount == 0) {
+            Q.stageScene("level1");
+          }
+        },
+
+        // When a dot is inserted, use it's parent (the stage)
+        // to keep track of the total number of dots on the stage
+        inserted: function() {
+          this.stage.dotCount = this.stage.dotCount || 0
+          this.stage.dotCount++;
+        }
+      });
+
+
+      // Tower is just a dot with a different sheet - use the same
+      // sensor and counting functionality
+      Q.Dot.extend("Tower", {
+        init: function(p) {
+          this._super(Q._defaults(p,{
+            sheet: 'tower'
+          }));
+        }
+      });
+});
